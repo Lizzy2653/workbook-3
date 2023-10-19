38a99bc2b8247fd0b79d5ec356a2a5854ef03a3b
Lizzy2653 <147071082+Lizzy2653@users.noreply.github.com>
2023-10-19 14:34:38 -0400

    save changes

--- employees/Main.java
+++ employees/Main.java
@@ -2,8 +2,25 @@ package org.example;
 public class Main {
     public employees(int id, name, hours worked, pay rate);
 
+    {"id": 10, "Dana Wyatt" : "52.5" : 12.50};
+    {"id": 20, "Ezra Aiden" : "17" : 16.75};
+    {"id": 30, "Brittany Thibbs" : "40" : 16.50"};
+    {"id": 40, "Zephaniah Huges" : "2" : 10.0};
+    {"id": 50, "Caylee Callahan" : "35" : 15.50};
+    {"id": 60, "Zayne Stephens" : "45" : 13.50};
+    {"id": 70, "Maverick Cummings" : "25.5" : 16.50};
+    {"id": 80, "Kamari Todd" : "32" :14.50};
+        }
 
+    }
     try {
+        // open the file
+        FileWriter - new FileWriter ('skills.txt');
+
+        // write to the file
+        writer.write("Skills:\n");
+        writer.write
+
 
         BufferedReader buffReader = new BufferedReader(
                 new FileReader (fileName "employees.csv"));
@@ -22,45 +39,5 @@ public class Main {
 
       }
    }
-
-       this.id = 10;
-       this.name = Dana Wyatt;
-       this.hours worked = 52.5;
-       this.pay rate = 12.50;
-
-       this.id = 20;
-       this.name = Ezra Aiden;
-       this.hours worked = 17;
-       this.pay rate = 16.75;
-
-       this.id = 30;
-       this.name = Brittany Thibbs;
-       this.hours worked = 40;
-       this.pay rate = 16.50;
-
-       this.id = 40;
-       this.name = Zephaniah Huges;
-       this.hours worked = 2;
-       this.pay rate = 10.0;
-
-       this.id = 50;
-       this.name = Caylee Callahan;
-       this.hours worked = 35;
-       this.pay rate = 15.50;
-
-       this.id = 60;
-       this.name = Zane Stephens;
-       this.hours worked = 45;
-       this.pay rate = 13.50;
-
-       this.id =70;
-       this.name = Maverick Cummings;
-       this.hours worked = 25.5;
-       this.pay rate = 16.50;
-
-       this.id =80;
-       this.name = Kamari Todd;
-       this.hours worked = 32;
-       this.pay rate = 14.50;
 
 }
