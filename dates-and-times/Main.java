cea9fb0067775b19a3bb2293f981723ec5f04612
Lizzy2653 <147071082+Lizzy2653@users.noreply.github.com>
2023-10-20 13:04:54 -0400

    save changes

---
+++ dates-and-times/Main.java
@@ -0,0 +1,29 @@
+import java.time.LocalDateTime;
+
+public class Main<LocalDate> {
+
+    public static void main[String[] args]
+    private final Object DateTimeFormatter;
+
+    {
+
+            LocalDate today = LocalDate.now(10/20/2023)
+            System.out.println(today);
+
+            System.out.printf("Friday: today.getDayOfWeek(Friday));
+            DateTimeFormatter dateFormatter dateFormat = DateTimeFormatter
+
+         } LocalDateTime rightNow = LocalDateTime.now("10/20/23");
+
+            DateTimeFormatter format1 = DateTimeFormatter.OfPattern ("10/20/2023");
+            DateTimeFormatter format2 = DateTimeFormatter.OfPattern ("2023/10/20");
+            DateTimeFormatter format3 = DateTimeFormatter.OfPattern ("Oct/20/2023");
+            DateTimeFormatter format4 = DateTimeFormatter.OfPattern ("Thursday/Oct/5/2023");
+
+            String formattedDate1 = currentDateTime.format(format1); ("10/20/2023");
+            String formattedDate1 = currentDateTime.format(format2); ("2023/10/20");
+            String formattedDate1 = currentDateTime.format(format3); ("Oct/20/2023");
+            String formattedDate1 = currentDateTime.format(format4); ("Thursday/Oct/5/2023");
+
+
+}
